ae327b2d0f2b680e5466f2abd45b77cc3cfdacda
PekkaLam <49144024+Tooli11@users.noreply.github.com>
2026-06-30 23:55:47 +0300

    healthcheck toimiva versio

--- app.js
+++ app.js
@@ -7,20 +7,21 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('3') // change this string to ensure a new version deployed
+  res.send('4') // change this string to ensure a new version deployed
 })
-/*Toimiva
+//Toimiva
 app.get('/health', (req, res) => {
   res.send('ok')
 })
-*/
+
 
 // Ei toimiva
+/*
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
   if (true) throw('error...  ')
   res.send('ok')
-})
+})*/
 
 
 const start = async () => {
